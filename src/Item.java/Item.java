567278d83a6aed17007271c44eca2c128a37558f
ablina08 <albinashabanaj12@gmail.com>
2026-03-16 12:02:44 +0100

    Add: Item.java code to represent a lost/found item

--- src/Item.java/Item.java
+++ src/Item.java/Item.java
@@ -9,4 +9,40 @@ public class Item {
         this.city = city;
         this.description = description;
     }
+
+      // getters
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
+
