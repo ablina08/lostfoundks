00cca37889d6c61fe3fb875b2652654a8268e9c1
ablina08 <albinashabanaj12@gmail.com>
2026-03-16 11:42:15 +0100

    Shtim teksti

--- src/Main.java/Main.java
+++ src/Main.java/Main.java
@@ -11,3 +11,20 @@ public class Main {
         System.out.println("City: " + lostItem.city);
     }
 }
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> lostItems = new ArrayList<>();
+
+        System.out.println("Lost & Found KS");
+        System.out.print("Enter lost item name: ");
+        String item = scanner.nextLine();
+        lostItems.add(item);
+
+        System.out.println("Items in list: " + lostItems);
+    }
+}
